3114e44303a6132509988005cfdd87fe0485027f
Hancock <hancock@creativekode.com>
2013-10-17 23:14:27 -0400

    comment route patch

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Pistoncms::Engine.routes.draw do
     resources :posts
   end
 
+  # Notice both page and post use same url
+  # Constraint on page enables post route to be called next
   get ':slug' => 'public/pages#show', as: "public_page", constraints: RouteConstraint.new
   get ':slug' => 'public/posts#show', as: "public_post"
 
