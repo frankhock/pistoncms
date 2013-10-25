f50b7b77c7ff9256539fd7085f402627858cf6c1
Hancock <hancock@creativekode.com>
2013-10-24 20:00:14 -0400

    fix css add ckeditor functionality

--- app/assets/javascripts/pistoncms/ckeditor/config.js
+++ app/assets/javascripts/pistoncms/ckeditor/config.js
@@ -6,7 +6,10 @@ CKEDITOR.config.toolbar= [
                     { name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source' ] },
                     { name: 'others', items: [ '-' ] },
                     { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Strike', '-', 'RemoveFormat' ] },
-                    { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote' ] },
-                    { name: 'styles', items: [ 'Format' ] },
-                    { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Scayt' ] }
+                    { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-', 'JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock' ] },
+                    { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Scayt' ] },
+                    '/',
+                    { name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
+                    { name: 'colors', items : [ 'TextColor','BGColor' ] },
+                    { name: 'tools', items : [ 'Maximize', 'ShowBlocks','-','About' ] }
                   ]
