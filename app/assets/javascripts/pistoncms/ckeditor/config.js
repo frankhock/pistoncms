6d3120cc9e92b3fba68f324a36585098ead4d565
Hancock <hancock@creativekode.com>
2013-10-28 22:32:30 -0400

    update css of ckeditor

--- app/assets/javascripts/pistoncms/ckeditor/config.js
+++ app/assets/javascripts/pistoncms/ckeditor/config.js
@@ -1,16 +1,13 @@
-UPLOADCARE_PUBLIC_KEY = "e2dc3ae396e76af502b5" // Uploadcare public key
-CKEDITOR.config.extraPlugins = 'uploadcare'
 CKEDITOR.config.allowedContent = true // Allow source code to be edited
 CKEDITOR.config.toolbar= [
-                    { name: 'insert', items: [ 'Uploadcare' ] },
-                    { name: 'tools', items: [ 'Maximize' ] },
+                    { name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
                     { name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source' ] },
                     { name: 'others', items: [ '-' ] },
                     { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Strike', '-', 'RemoveFormat' ] },
                     { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-', 'JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock' ] },
                     { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Scayt' ] },
-                    '/',
                     { name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
                     { name: 'colors', items : [ 'TextColor','BGColor' ] },
-                    { name: 'tools', items : [ 'Maximize', 'ShowBlocks','-','About' ] }
+                    { name: 'tools', items : [ 'About' ] }
                   ]
+CKEDITOR.config.height = '400px';
